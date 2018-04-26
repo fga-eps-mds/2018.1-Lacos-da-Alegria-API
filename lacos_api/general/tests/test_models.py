15acb12c5352e0fd28be3240396a44bfb8d41edf
Rômulo Vinícius de Souza <romulovinicius10@gmail.com>
2018-04-26 15:48:28 -0300

    Created TestActivity in test_models.
    Signed-off-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com>
    Signed-off-by: Luciano dos Santos <luciano_z7@hotmail.com>
    
    Co-authored-by: Luciano dos Santos <luciano_z7@hotmail.com>
    Co-authored-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com>

--- lacos_api/general/tests/test_models.py
+++ lacos_api/general/tests/test_models.py
@@ -1,5 +1,6 @@
 from test_plus.test import TestCase
-
+from ..models import Activity
+import pytest
 
 class TestUser(TestCase):
 
@@ -14,3 +15,30 @@ class TestUser(TestCase):
 
     # def test_get_absolute_url(self):
     #     self.assertEqual(self.user.get_absolute_url(), "/profiles/testuser/")
+
+class TestActivity(TestCase):
+
+    def setUp(self):
+        return Activity.objects.create(
+            name="hospGama", 
+            volunteers="30", 
+            limit="True",
+            #created="2018-04-19T12:01:09.752144Z",
+            status="1",
+            #time="2000-12-12",
+            duration="60",
+            subscription="True",
+            call="True" 
+        )
+
+    def test_create_activity(self):
+        w = self.setUp()
+        self.assertEqual(w.name,"hospGama")
+        self.assertEqual(w.volunteers,"30")
+        self.assertEqual(w.limit,"True")
+        self.assertEqual(w.status,"1")
+        self.assertEqual(w.duration,"60")
+        self.assertEqual(w.subscription,"True")
+        self.assertEqual(w.call,"True")
+        
+        
