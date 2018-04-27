6e8649af80e8c643e045d3dbc2b0bb2b6cb9b2ec
joaovitorml <joao.lemos4000@gmail.com>
2018-04-27 06:29:24 -0300

    Fixed errors in test_user. Signed-off-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com> Signed-of-by: Rafael Makaha <rafael.makaha@gmail.com> Signed-of-br: João Vítor Morandi <joao.lemos4000@gmail.com>
    Co-authored-by: João Vítor Morandi <joao.lemos4000@gmail.com>
    Co-authored-by: Rafael Makaha <rafael.makaha@gmail.com>
    Co-authored-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com>

--- lacos_api/general/tests/test_models.py
+++ lacos_api/general/tests/test_models.py
@@ -1,11 +1,12 @@
 from test_plus.test import TestCase
+from django.db import transaction
 from ..models import UserProfile,Activity
-from django.db import models
+#from django.db import models
 
-class TestUser(TestCase):
-
-    def setUp(self):
-        self.user = self.make_user()
+# class TestUser(TestCase):
+#
+#     def setUp(self):
+#         self.user = self.make_user()
 
     # def test__str__(self):
     #     self.assertEqual(
@@ -15,14 +16,54 @@ class TestUser(TestCase):
 
     # def test_get_absolute_url(self):
     #     self.assertEqual(self.user.get_absolute_url(), "/profiles/testuser/")
-   
+
 class TestUserProfile(TestCase):
-    def setUp(self):
-        return UserProfile.objects.create(
+    # def test_constraint(self):
+    #     try:
+    #         # Duplicates should be prevented.
+    #         with transaction.atomic():
+    #             UserProfile.objects.create(
+    #                 username="ZecaPagodinho",
+    #                 password="abc12345",
+    #                 email="testeeee@teste.com",
+    #                 cpf="2469666",
+    #                 name="zecapagodinho",
+    #                 birth= "2018-04-26",
+    #                 region="cataratas",
+    #                 preference="deus",
+    #                 ddd="11",
+    #                 whatsapp="40028922",
+    #                 address="casa",
+    #                 howDidYouKnow="pericles",
+    #                 want_ongs="True"
+    #                 )
+    #             self.fail('Duplicate question allowed.')
+    #     except IntegrityError:
+    #             pass
+    # def setUp(self):
+    #     with transaction.atomic():
+    #         return UserProfile.objects.create(
+    #             username="ZecaPagodinho",
+    #             password="abc12345",
+    #             email="testeeee@teste.com",
+    #         #    cpf="246966600",
+    #             name="zecapagodinho",
+    #             birth= "2018-04-26",
+    #             region="cataratas",
+    #             preference="deus",
+    #             ddd="11",
+    #             whatsapp="40028922",
+    #             address="casa",
+    #             howDidYouKnow="pericles",
+    #             want_ongs="True"
+    #        )
+    def test_create_user(self):
+        # user1 = self.setUp()
+        user1 = UserProfile.objects.create(
             username="ZecaPagodinho",
-            password="ClaytuRasta",
-            email="teste@teste.com",
-            cpf="2469666",
+            password="12345abc",
+            email="testeeee@teste.com",
+            cpf="246966600",
             name="zecapagodinho",
             birth= "2018-04-26",
             region="cataratas",
@@ -32,18 +73,16 @@ class TestUserProfile(TestCase):
             address="casa",
             howDidYouKnow="pericles",
             want_ongs="True"
-       )
-    def test_create_user(self):
-        user_test = self.setUp()
-        self.assertEqual(user_test.username,"ZecaPagodinho")
-        self.assertEqual(user_test.password,"ClaytuRasta")
-        self.assertEqual(user_test.email,"teste@teste.com")
-        self.assertEqual(user_test.cpf,"2469666")
-        self.assertEqual(user_test.birth,"2018-04-26")
-        self.assertEqual(user_test.region,"cataratas")
-        self.assertEqual(user_test.preference,"deus")
-        self.assertEqual(user_test.ddd,"11")
-        self.assertEqual(user_test.whatsapp,"40028922")
-        self.assertEqual(user_test.address,"casa")
-        self.assertEqual(user_test.howDidYouKnow,"pericles")
-        self.assertEqual(user_test.want_ongs,"True")
+            )
+        self.assertEqual(user1.username,"ZecaPagodinho")
+        self.assertEqual(user1.password,"12345abc")
+        self.assertEqual(user1.email,"testeeee@teste.com")
+        self.assertEqual(user1.cpf,"246966600")
+        self.assertEqual(user1.birth,"2018-04-26")
+        self.assertEqual(user1.region,"cataratas")
+        self.assertEqual(user1.preference,"deus")
+        self.assertEqual(user1.ddd,"11")
+        self.assertEqual(user1.whatsapp,"40028922")
+        self.assertEqual(user1.address,"casa")
+        self.assertEqual(user1.howDidYouKnow,"pericles")
+        self.assertEqual(user1.want_ongs,"True")
