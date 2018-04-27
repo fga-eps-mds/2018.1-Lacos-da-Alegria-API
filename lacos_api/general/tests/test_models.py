6cd9cbef4fade59df389cbf982e25599b6d7296a
Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>
2018-04-26 22:18:49 -0300

    Created unit test for create_user.
    Signed-of-by: Rafael Makaha <rafael.makaha@gmail.com>
    Signed-of-br: João Vítor Morandi <joao.lemos4000@gmail.com>
    
    Co-authored-by: João Vítor Morandi <joao.lemos4000@gmail.com>

--- lacos_api/general/tests/test_models.py
+++ lacos_api/general/tests/test_models.py
@@ -1,5 +1,6 @@
 from test_plus.test import TestCase
-
+from ..models import UserProfile,Activity
+from django.db import models
 
 class TestUser(TestCase):
 
@@ -14,3 +15,35 @@ class TestUser(TestCase):
 
     # def test_get_absolute_url(self):
     #     self.assertEqual(self.user.get_absolute_url(), "/profiles/testuser/")
+   
+class TestUserProfile(TestCase):
+    def setUp(self):
+        return UserProfile.objects.create(
+            username="ZecaPagodinho",
+            password="ClaytuRasta",
+            email="teste@teste.com",
+            cpf="2469666",
+            name="zecapagodinho",
+            birth= "2018-04-26",
+            region="cataratas",
+            preference="deus",
+            ddd="11",
+            whatsapp="40028922",
+            address="casa",
+            howDidYouKnow="pericles",
+            want_ongs="True"
+       )
+    def test_create_user(self):
+        user_test = self.setUp()
+        self.assertEqual(user_test.username,"ZecaPagodinho")
+        self.assertEqual(user_test.password,"ClaytuRasta")
+        self.assertEqual(user_test.email,"teste@teste.com")
+        self.assertEqual(user_test.cpf,"2469666")
+        self.assertEqual(user_test.birth,"2018-04-26")
+        self.assertEqual(user_test.region,"cataratas")
+        self.assertEqual(user_test.preference,"deus")
+        self.assertEqual(user_test.ddd,"11")
+        self.assertEqual(user_test.whatsapp,"40028922")
+        self.assertEqual(user_test.address,"casa")
+        self.assertEqual(user_test.howDidYouKnow,"pericles")
+        self.assertEqual(user_test.want_ongs,"True")
