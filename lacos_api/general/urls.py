4997bbea9ec7b07172006703c600b28b995aa47a
Rômulo Vinícius de Souza <romulovinicius10@gmail.com>
2018-05-04 11:04:49 -0300

    Added simple jwt authentication.
    Signed-off-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com>
    Signed-off-by: Joao Vitor Lemos <joao.lemos4000@gmail.com>
    
    Co-authored-by: Joao Vitor Lemos <joao.lemos4000@gmail.com>
    Co-authored-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com>

--- lacos_api/general/urls.py
+++ lacos_api/general/urls.py
@@ -2,6 +2,11 @@ from django.conf.urls import url
 from django.conf.urls import include
 
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 from . import views
 
@@ -15,5 +20,8 @@ router.register('activities', views.ActivityViewSet)
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),
     url(r'^userprofile-view', views.UserProfileView.as_view()),
-    url(r'', include(router.urls))
+    url(r'', include(router.urls)),
+    url(r'^api/token/$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    url(r'^api/token/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
+    url(r'^api/token/verify/$', TokenVerifyView.as_view(), name='token_verify') 
 ]
