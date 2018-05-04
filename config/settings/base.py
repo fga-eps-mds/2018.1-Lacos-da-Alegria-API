f0da70efde4141b9abff486b424f82f21673b4b1
Rômulo Vinícius de Souza <romulovinicius10@gmail.com>
2018-05-04 11:12:47 -0300

    Added initial behavior to simple jwt.
    Signed-off-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com>
    Signed-off-by: Joao Vitor Lemos <joao.lemos4000@gmail.com>
    
    Co-authored-by: Joao Vitor Lemos <joao.lemos4000@gmail.com>
    Co-authored-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com>

--- config/settings/base.py
+++ config/settings/base.py
@@ -2,6 +2,8 @@
 Base settings to build other settings files upon.
 """
 
+from datetime import timedelta
+
 import environ
 import os
 
@@ -290,8 +292,35 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.FormParser',
         'rest_framework.parsers.MultiPartParser',
      ),
-    'DEFAULT_PERMISSION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
+
+# Customizing JWT's behavior
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+
+    'ALGORITHM': 'HS256',
+    #'SIGNING_KEY': settings.SECRET_KEY,
+    'VERIFYING_KEY': None,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
+
 
 CORS_ORIGIN_ALLOW_ALL = True
