270d32f3b3fd107f60c187d9357c6fd50aa85dc9
Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>
2018-05-25 11:57:42 -0300

    Changed authorization route.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>
    Signed-off-by: Joao Vitor <joao.lemos4000@gmail.com>
    Signed-off:by: Renan Schadt <welzrenan@gmail.com>
    
    Co-authored-by: Joao Vitor <joao.lemos4000@gmail.com>
    Co-authored-by: Renan Schadt <welzrenan@gmail.com>

--- config/settings/base.py
+++ config/settings/base.py
@@ -106,7 +106,7 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
-AUTH_USER_MODEL = 'general.UserProfile'
+AUTH_USER_MODEL = 'user_api.UserProfile'
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
 # LOGIN_REDIRECT_URL = 'users:redirect'
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-url
