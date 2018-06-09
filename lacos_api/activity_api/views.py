b83fdc6f9f30015a25a5cb5466c33b7c5be3c351
Sara Silva <saracsas2@gmail.com>
2018-06-08 23:27:52 -0300

    Created sort method. Signed-off-by: Luciano Santos <luciano_z7@hotmail.com> Signed-off-by: Sara Silva <saracsas2@gmail.com>
    Co-authored-by: Luciano Santos <luciano_z7@hotmail.com>

--- lacos_api/activity_api/views.py
+++ lacos_api/activity_api/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response 
 
 from . import serializers, models
 
@@ -6,6 +8,13 @@ from . import serializers, models
 class HospitalActivityViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.HospitalActivitySerializer
     queryset = models.HospitalActivity.objects.all()
+
+    @action(methods=['get'], detail=True)
+    def sort(self, request, pk=None):
+        activity = models.HospitalActivity.objects.get(pk=pk)
+        volunteers = [user.id for user in activity.userprofile_set.all()]
+        print(volunteers)
+        return Response({'oi': str(volunteers)}, status.HTTP_200_OK)
 
 
 class NGOActivityViewSet(viewsets.ModelViewSet):
