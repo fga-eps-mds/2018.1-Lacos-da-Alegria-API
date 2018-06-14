52e95f37a1e27bfb342c8a8646b97a6f867408a2
Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>
2018-06-13 21:36:20 -0300

    Fixed lottery and created unsubscribe functions.
    Signed-off-by: Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>

--- lacos_api/activity_api/views.py
+++ lacos_api/activity_api/views.py
@@ -39,17 +39,48 @@ class HospitalActivityViewSet(viewsets.ModelViewSet):
     def lottery(self, request, pk=None):
         sorteados = []
         espera = []        
+
         activity = self.queryset.get(pk=pk)
         volunteers = [user.id for user in activity.prelis.all()]
         random.shuffle(volunteers)
+
         for i in volunteers:
             sorteados = volunteers[0:activity.volunteers]
             espera = volunteers[activity.volunteers:len(volunteers)]
+
         activity.selected = ''.join(str(sorteados))
         activity.waiting = ''.join(str(espera))
-        print("aqui",activity.selected)
-        # activity.selected = [1,2,3]
-        return Response({'sorteadddosss': activity.waiting}, status.HTTP_200_OK)
+
+        activity.save()
+
+        return Response({'sorteadddosss': activity.selected}, status.HTTP_200_OK)
+
+    @action(methods=['get'], detail=True)
+    def unsubscribe(self,request, pk=None):
+        activity_pk = pk
+        user_pk = request.query_params.get('user_key', None)
+
+        user = UserProfile.objects.get(pk=user_pk)
+        activity = self.queryset.get(pk=activity_pk)
+        response = Response({'status': 'User was not subscribed'}, status.HTTP_200_OK)
+
+        if user in activity.prelis.all():
+            activity.prelis.remove(user)
+            selected = [int(n) for n in activity.selected.split(',')]
+            if user.id in selected:
+                selected.remove(user.id)
+                selected = ','.join(str(n) for n in selected)
+                activity.selected = selected
+
+                response = Response({'status': 'Succesfully deleted'}, status.HTTP_200_OK)
+            else:
+                waiting = [int(n) for n in activity.waiting.split(',')]
+                waiting.remove(user.id)
+                activity.waitin = waiting
+
+                response = Response({'status': 'Succesfully deleted'}, status.HTTP_200_OK)
+
+        return response
 
 
 class NGOActivityViewSet(viewsets.ModelViewSet):
