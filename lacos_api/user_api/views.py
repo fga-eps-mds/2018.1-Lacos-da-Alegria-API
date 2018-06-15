d68a05fa92402bf6eb5bef19cfe5d6d253c41332
silvasara <saracsas2@gmail.com>
2018-06-15 00:38:07 -0300

    Created edit profile method. Signed-off-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com> Signed-off-by: Sara Silva <saracsas2@gmail.com>
    Co-authored-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com>

--- lacos_api/user_api/views.py
+++ lacos_api/user_api/views.py
@@ -1,4 +1,5 @@
 from django.utils import timezone
+from django.contrib.auth.models import BaseUserManager
 from datetime import timedelta
 from rest_framework import viewsets, status
 from rest_framework.response import Response
@@ -28,6 +29,21 @@ class UserProfileViewSet(viewsets.ModelViewSet):
             response = Response({'status': 'Succesfully deleted'}, status.HTTP_200_OK)
         else:
             response = Response({'error': 'Passwords do not match'}, status.HTTP_403_FORBIDDEN)
+
+        return response
+    
+    @action(methods=['post'], detail=True)
+    def edit_user(self, request, pk=None):
+        data = request.data
+        password = data.get('password')
+        user = models.UserProfile.objects.get(pk=pk)
+        if user.check_password(password):
+            response = Response({'error': 'A senha nova coincide com a senha antiga'}, status.HTTP_403_FORBIDDEN)
+        else:
+            user.set_password(password)
+            user.save()
+            print(user)
+            response = Response({'status': 'Passwords do not match'}, status.HTTP_200_OK)
 
         return response
 
